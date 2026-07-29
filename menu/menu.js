64009200b63e2ad453b19130654a78664703e955
verner <verner.bu@gmail.com>
2026-07-29 07:37:34 +0700

    Update menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -22,24 +22,29 @@ document.body.insertAdjacentHTML("beforeend", `
 </a>
 
 
-<a class="nx-item" href="../preschool/index.html">
+<div class="nx-item nx-section" data-section="preschool">
 🧸 Дошколятам
-</a>
+</div>
 
+<div class="nx-submenu" id="submenu-preschool"></div>
 
-<a class="nx-item" href="../reading/index.html">
+<div class="nx-item nx-section" data-section="reading">
 📖 Скорочтение
-</a>
+</div>
 
+<div class="nx-submenu" id="submenu-reading"></div>
 
-<a class="nx-item" href="../math/index.html">
+<div class="nx-item nx-section" data-section="math">
 🧮 Скоросчёт
-</a>
+</div>
 
+<div class="nx-submenu" id="submenu-math"></div>
 
-<a class="nx-item" href="../other/index.html">
+<div class="nx-item nx-section" data-section="other">
 🎲 Другое
-</a>
+</div>
+
+<div class="nx-submenu" id="submenu-other"></div>
 
 
 <hr>
@@ -68,6 +73,72 @@ const drawer = document.getElementById("neurox-drawer");
 const overlay = document.getElementById("neurox-overlay");
 const button = document.getElementById("neurox-menu-btn");
 const fullscreenButton = document.getElementById("fullscreen-btn");
+
+const MENU = {
+
+    preschool: [
+        // пока пусто
+    ],
+
+    reading: [
+        {icon:"📋", title:"Шульте", file:"Shulte.html"},
+        {icon:"📖", title:"Читалка", file:"chitaem/chitalka.html"},
+        {icon:"🅰", title:"Алфавит", file:"alfabet.html"},
+        {icon:"🧠", title:"Анаграммы", file:"putal.html"},
+        {icon:"🌈", title:"Радуга", file:"raduga.html"},
+        {icon:"🔄", title:"Перевёртыши", file:"pereverton.html"},
+        {icon:"🐌", title:"Столбики", file:"stolbiky/stolby.html"},
+        {icon:"🏔", title:"Лавина", file:"bystro_chitaem/lavina.html"},
+        {icon:"🔤", title:"Слоги", file:"slogi/index.html"}
+    ],
+
+    math: [
+        {icon:"🏠", title:"Домики", file:"domiky.html"},
+        {icon:"🔎", title:"Найди число", file:"findnumber.html"},
+        {icon:"🔲", title:"Магический квадрат", file:"magic.html"},
+        {icon:"🧮", title:"Считалочка", file:"primery.html"},
+        {icon:"✖", title:"Таблица умножения", file:"tablitsa.html"},
+        {icon:"⚫", title:"Точки", file:"tochki.html"}
+    ],
+
+    other: [
+        {icon:"🌀", title:"Лабиринт", file:"labyrint.html"},
+        {icon:"🃏", title:"Мемо", file:"memo.html"},
+        {icon:"👀", title:"Что изменилось?", file:"peremeny.html"},
+        {icon:"🚦", title:"Светофор", file:"stoplight.html"}
+    ]
+
+};
+
+function buildMenu(){
+
+    Object.keys(MENU).forEach(section=>{
+
+        const container = document.getElementById("submenu-"+section);
+
+        if(!container) return;
+
+        container.innerHTML = "";
+
+        MENU[section].forEach(item=>{
+
+            const link = document.createElement("a");
+
+            link.className = "nx-subitem";
+
+            link.innerHTML = `${item.icon} ${item.title}`;
+
+            link.href = item.file;
+
+            container.appendChild(link);
+
+        });
+
+    });
+
+}
+
+buildMenu();
 
 
 
